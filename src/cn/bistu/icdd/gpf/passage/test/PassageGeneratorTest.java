9f967fa5309afa91dcaeab4559a0ecd88245c3af
flygoinggoing <929475257@qq.com>
2017-04-08 15:39:54 +0800

    添加测试

---
+++ src/cn/bistu/icdd/gpf/passage/test/PassageGeneratorTest.java
@@ -0,0 +1,10 @@
+package cn.bistu.icdd.gpf.passage.test;
+
+import cn.bistu.icdd.gpf.passage.PassageGenerator;
+
+public class PassageGeneratorTest {
+	public static void main(String[] args) {
+		PassageGenerator pg = new PassageGenerator("C:/Users/92947/Desktop/autotest");
+		
+	}
+}
